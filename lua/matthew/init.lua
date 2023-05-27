f26a2651db51eac76d00e6c5571dd00c21437db4
xxmatthewandersonxx <mjanderson1227@gmail.com>
2023-05-27 16:39:19 -0500

    Death and destruction

--- lua/matthew/init.lua
+++
@@ -1,3 +0,0 @@
-require('matthew.remap')
-require('matthew.settings')
-require('matthew.packer')
--- lua/matthew/remap.lua
+++
@@ -1,7 +0,0 @@
-vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pr", vim.cmd.RnvimrToggle)
-vim.keymap.set("n", "<leader>nt", vim.cmd.Neotree)
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set("n", "<leader>n", vim.cmd.bnext)
-vim.keymap.set("n", "<leader>p", vim.cmd.bprevious)
-vim.keymap.set("n", "<leader>d", vim.cmd.bdelete)
