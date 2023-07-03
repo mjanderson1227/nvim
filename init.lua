ac56e0dcd0c485819398baf89b2ccb2f8b5b3d58
xxmatthewandersonxx <92887765+mjanderson1227@users.noreply.github.com>
2023-07-03 16:45:18 -0500

    Fixed html issue again

--- init.lua
+++ init.lua
@@ -686,6 +686,9 @@ mason_lspconfig.setup {
 mason_lspconfig.setup_handlers {
   function(server_name)
     require('lspconfig')[server_name].setup {
+      init_options = {
+        provideFormatter = true
+      },
       capabilities = capabilities,
       on_attach = on_attach,
       settings = servers[server_name],
